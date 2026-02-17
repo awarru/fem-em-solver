27881291eed220cfeb8b27434ebd57debd33ad41
Awarru Tark <awarru@gmail.com>
2026-02-17 20:07:00 +0000

    Fix Docker setup and imports
    Docker Compose fixes:
    - Removed obsolete 'version' attribute from docker-compose.yml
    - Added /usr/local/dolfinx-real/lib/python3.10/dist-packages to PYTHONPATH
    - Added /usr/local/lib to PYTHONPATH for gmsh
    - Fixed pyproject.toml: removed empty email from authors
    
    Package fixes:
    - Removed imports for non-existent classes (TimeHarmonicSolver, etc.)
    - Package now imports cleanly in Docker container
    
    Tested: All imports working (dolfinx, gmsh, fem_em_solver)

--- src/fem_em_solver/__init__.py
+++ src/fem_em_solver/__init__.py
@@ -8,19 +8,8 @@ __version__ = "0.1.0"
 __author__ = "Awarru"
 
 # Core imports
-from .core.solvers import MagnetostaticSolver, TimeHarmonicSolver
-from .core.function_spaces import create_function_spaces
-
-# Coil imports
-from .coils.loop import CircularLoop
-
-# Material imports
-from .materials.phantoms import GelledSalinePhantom
+from .core.solvers import MagnetostaticSolver
 
 __all__ = [
     "MagnetostaticSolver",
-    "TimeHarmonicSolver", 
-    "create_function_spaces",
-    "CircularLoop",
-    "GelledSalinePhantom",
 ]
